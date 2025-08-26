9809da2fc5dd3a9201047d666684b9a5f73c1d68
MandapatiVamsikrishna <vtu16968@veltech.edu.in>
2025-08-26 12:55:42 +0100

    Create Product.js

---
+++ Product.js
@@ -0,0 +1,7 @@
+[
+  { "id": "1", "name": "Product 1", "category": "electronics", "price": 20, "img": "https://via.placeholder.com/200" },
+  { "id": "2", "name": "Product 2", "category": "apparel",     "price": 25, "img": "https://via.placeholder.com/200" },
+  { "id": "3", "name": "Product 3", "category": "home",         "price": 30, "img": "https://via.placeholder.com/200" },
+  { "id": "4", "name": "Red Apples", "category": "Fruits",         "price": 2.99, "img": "https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?q=80&w=800&auto=format&fit=crop" }
+  /* Add ALL your site products here (from other pages too) */
+]
